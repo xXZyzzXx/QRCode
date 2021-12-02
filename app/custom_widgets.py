dac5c1361524ce9d6a9d19d4b15ebdfff0159efd
zyzzwombat <vladyslav.belous@gowombat.team>
2021-12-02 16:41:09 +0200

    completed register screen

--- app/custom_widgets.py
+++ app/custom_widgets.py
@@ -1,6 +1,10 @@
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.clock import Clock
 
 from app.utils import screen_manager
+from app.database import create_new_pupil
 
 
 class MenuButton(Button):
@@ -12,4 +16,71 @@ class MenuButton(Button):
         screen_manager.current = self.text
 
 
+class OutlinedLabel(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.outline_width = 2
+        self.outline_color = (.7, .7, .7)
 
+
+class CustomTextInput(TextInput):
+    def __init__(self, key: str, **kwargs):
+        super().__init__(**kwargs)
+        self.key = key
+
+
+class RegisterNewPupilButton(Button):
+    """
+    Save pupil data to database and show result notification
+    """
+    def __init__(self, text_inputs: list, nl: Label, **kwargs):
+        super().__init__(**kwargs)
+        self.text = "Create new pupil"
+        self.text_inputs = text_inputs
+        self.notification_label = nl
+
+    def on_release(self):
+        self.validate_text_input_data()
+
+    def validate_text_input_data(self) -> None:
+        pupil_data = {}
+        missed_fields = []
+        # Checking is all text input fields valid
+        for text_input in self.text_inputs:
+            line_text = text_input.text
+            if not line_text:
+                missed_fields.append(text_input.key)
+            else:
+                pupil_data[text_input.key] = text_input.text
+        # Show notification result and save to database
+        if missed_fields:
+            self.show_fail_message(missed_fields=missed_fields)
+        else:
+            self.save_pupil_data_to_database(data=pupil_data)
+            self.show_success_message()
+        # Remove notification after specified time
+        Clock.schedule_once(self.notification_remove_callback, 3)
+
+    def save_pupil_data_to_database(self, data: dict) -> None:
+        name = data.get("name")
+        first_name = data.get("first_name")
+        phone_number = data.get("phone_number")
+        create_result = create_new_pupil(name=name, first_name=first_name, phone_number=phone_number)
+        if not create_result:
+            self.show_fail_message(extra_message="Save pupils to database error !")
+
+    def show_success_message(self) -> None:
+        self.notification_label.color = (0, 1, 0)
+        self.notification_label.text = "You have successfully created a new pupil !"
+
+    def show_fail_message(self, missed_fields: list = None, extra_message: str = None) -> None:
+        self.notification_label.color = (1, 0, 0)
+        fail_message = f"You need to fill this fields: {', '.join([*missed_fields])}"
+        print(f"Missed fields: {missed_fields}")
+        if extra_message:
+            fail_message = extra_message
+        self.notification_label.text = fail_message
+
+    def notification_remove_callback(self, dt: float) -> None:
+        """Remove notification after showing"""
+        self.notification_label.text = ""
--- app/screens.py
+++ app/screens.py
@@ -1,10 +1,13 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
 from kivy_garden.zbarcam import ZBarCam
 
 from app.utils import Screens, build_screen
-from app.custom_widgets import MenuButton
+from app.custom_widgets import MenuButton, RegisterNewPupilButton, OutlinedLabel, CustomTextInput
 
 
 class MenuScreen(Screen):
@@ -21,11 +24,48 @@ class MenuScreen(Screen):
 
 # Screens
 class RegisterScreen(Screen):
+    """
+    Screen to register new pupils
+    """
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.text_input_lines = None
+
     def on_enter(self, *args):
+        self.text_input_lines = []  # Refresh initial data
         body = BoxLayout()
-        # Custom logic
+        # Creating register menu
+        body.add_widget(self.build_register_menu())
+        # Building all parts together
         root = build_screen(screen_name=self.name, body=body)
         self.add_widget(root)
+
+    def build_register_menu(self):
+        root = AnchorLayout(anchor_x="center", anchor_y="top")
+
+        notifications_lay = BoxLayout(orientation="vertical", height=50)
+        notification_label = OutlinedLabel()
+        notifications_lay.add_widget(notification_label)  # Button(text="notifications")
+
+        box_lay = BoxLayout(orientation="vertical", size_hint=(0.7, 0.8), padding=(0, 0, 0, 50))
+        box_lay.add_widget(notifications_lay)
+        box_lay.add_widget(Label(text=r"You can create a new pupil here \/", halign="left"))
+        # List with required fields to create a new pupil
+        required_fields = ["name", "first name", "phone number"]
+        # Generating labels and text inputs for each line
+        for line_name in required_fields:
+            line_box = BoxLayout(orientation="vertical")
+            label = Label(text=line_name)
+            text_input = CustomTextInput(key=line_name)
+            self.text_input_lines.append(text_input)  # Add to global list
+            line_box.add_widget(label)
+            line_box.add_widget(text_input)
+            box_lay.add_widget(line_box)
+
+        box_lay.add_widget(Widget(size_hint_y=None, height=50))  # Empty widget as separator
+        box_lay.add_widget(RegisterNewPupilButton(text_inputs=self.text_input_lines, nl=notification_label))
+        root.add_widget(box_lay)
+        return root
 
 
 class CalendarScreen(Screen):
