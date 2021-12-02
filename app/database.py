6e84a94ad07ba0df6e668a23eeaf30089ce4850e
zyzzwombat <vladyslav.belous@gowombat.team>
2021-12-02 15:15:18 +0200

    add database methods for pupils

---
+++ app/database.py
@@ -0,0 +1,50 @@
+from uuid import uuid4
+from typing import Optional, Generator
+from json.decoder import JSONDecodeError
+
+from kivy.storage.jsonstore import JsonStore
+
+
+pupils_store = JsonStore('db/pupils_db.json')
+
+
+def create_new_pupil(name: str, first_name: str, phone_number: str) -> bool:
+    try:
+        pupil_id = str(uuid4())
+        pupil_data = {
+            "pupil_id": pupil_id,
+            "name": name,
+            "first_name": first_name,
+            "phone_number": phone_number,
+        }
+        pupils_store.put(
+            name,
+            **pupil_data,
+        )
+        return True
+    except JSONDecodeError as e:
+        print(f"Create new pupil error: {e}")
+        return False
+
+
+def get_pupil_data_by_name(pupil_name: str) -> Optional[dict]:
+    try:
+        pupil_data = None
+        find_result: Generator = pupils_store.find(name=pupil_name)
+        for item in find_result:
+            # item is a tuple of key and value
+            if item:
+                pupil_data = item[1]
+                break
+        return pupil_data
+    except Exception as e:
+        import traceback
+        print(f"Get pupil data error: {e}, {traceback.format_exc()}")
+
+
+def get_all_pupils() -> Optional[dict]:
+    try:
+        all_pupils = dict(pupils_store)
+        return all_pupils
+    except Exception as e:
+        print(f"Create new pupil error: {e}")
---
+++ tests.py
@@ -0,0 +1,13 @@
+from app.database import create_new_pupil, get_all_pupils, get_pupil_data_by_name
+
+
+create_new_pupil(name="Test name", first_name="Test", phone_number="+380059430593")
+create_new_pupil(name="Username", first_name="Test2", phone_number="+3843258237834")
+
+pupil_by_name = get_pupil_data_by_name(pupil_name="Username")
+print(f"pupil_by_name: {pupil_by_name}\n")
+
+all_pupils = get_all_pupils()
+
+for pupil in all_pupils:
+    print(f"name: {pupil}, data: {all_pupils[pupil]}")
